ae4e8f795b60594e22fb26024807e6d394b6107f
javiergranadosr <javiergranadosrdev@gmail.com>
2025-01-19 14:22:03 -0600

    Se crea proyecto base para la app de inventario, con su configuracion de rutas inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        (d) => d.DashboardModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, useHash: true})],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
